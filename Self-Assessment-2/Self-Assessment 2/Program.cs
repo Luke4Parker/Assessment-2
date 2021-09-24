c4159908c5d95919227736b20300bfb00dea0e38
Luke Parker <lukeparke0@gmail.com>
2021-09-24 13:26:38 -0500

    Uploading Self-Assessment-2, completed all but number 8

---
+++ Self-Assessment-2/Self-Assessment 2/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace Self_Assessment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Chapter 3
+            /****************************/
+
+            //Exercise 3
+
+            //Console.WriteLine("Enter an integer: ");
+            //string input = Console.ReadLine();
+            //int num1 = int.Parse(input);
+
+            //int num2 = num1 / 100;
+            //if (num2 % 10 == 7)
+            //{
+            //    Console.WriteLine(num1);
+            //}
+
+            //Exercise 7
+
+            //Console.WriteLine("Enter your weight: ");
+            //string input = Console.ReadLine();
+            //double weight = double.Parse(input);
+
+            //double moonWeight = ((double)weight * .17);
+            //Console.WriteLine("Your weight on the moon is " + moonWeight);
+
+            /********************************/
+            //Chapter 5
+            /********************************/
+
+            //Exercise 2
+
+            //Console.WriteLine("Enter a real number: ");
+            //string input = Console.ReadLine();
+            //double num1 = double.Parse(input);
+            //Console.WriteLine("Enter a real number: ");
+            //input = Console.ReadLine();
+            //double num2 = double.Parse(input);
+            //Console.WriteLine("Enter a real number: ");
+            //input = Console.ReadLine();
+            //double num3 = double.Parse(input);
+
+            //if (num1 == 0 || num2 == 0 || num3 == 0)
+            //{
+            //    Console.WriteLine('+');
+            //}
+            //else if (num1 > 0 && num2 > 0 && num3 > 0)
+            //{
+            //    Console.WriteLine('+');
+            //}
+            //else if (num1 < 0 && num2 < 0 && num3 < 0)
+            //{
+            //    Console.WriteLine('-');
+            //}
+            //else if (num1 < 0 && num2 > 0 && num3 > 0)
+            //{
+            //    Console.WriteLine('-');
+            //}
+            //else if (num1 > 0 && num2 < 0 && num3 > 0)
+            //{
+            //    Console.WriteLine('-');
+            //}
+            //else if (num1 > 0 && num2 > 0 && num3 < 0)
+            //{
+            //    Console.WriteLine('-');
+            //}
+            //else if (num1 > 0 && num2 < 0 && num3 < 0)
+            //{
+            //    Console.WriteLine('+');
+            //}
+            //else if (num1 < 0 && num2 < 0 && num3 > 0)
+            //{
+            //    Console.WriteLine('+');
+            //}
+            //else if (num1 < 0 && num2 > 0 && num3 < 0)
+            //{
+            //    Console.WriteLine('+');
+            //}
+
+            //Exercise 5
+
+            //Console.WriteLine("Enter a digit: ");
+            //string input = Console.ReadLine();
+            //int num1 = int.Parse(input);
+
+            //while (num1 < 0 || num1 > 9)
+            //{
+            //    Console.WriteLine("You've entered an incorrect number.\nEnter a digit from 0 to 9: ");
+            //    input = Console.ReadLine();
+            //    num1 = int.Parse(input);
+            //}
+            //switch (num1)
+            //{
+            //    case 0:
+            //        Console.WriteLine("Zero");
+            //        break;
+            //    case 1:
+            //        Console.WriteLine("One");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Two");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Three");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Four");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Five");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Six");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Seven");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("Eight");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Nine");
+            //        break;
+            }
+
+            //Exercise 8
+
+
+        }
+    }
+}
